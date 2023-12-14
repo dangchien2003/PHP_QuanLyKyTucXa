23859a4e691edcebbc79aca9ae78216c2dba1860
dangchien2003 <chienboy03@gmail.com>
2023-12-15 03:28:40 +0700

    setup

---
+++ project/public/js/login.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(".bi-eye-slash-fill").hide();
+    $(".bi-eye-fill").click(function() {
+        $(".bi-eye-fill").hide();
+        $(".bi-eye-slash-fill").fadeIn();
+        $("#pass-input").attr('type', "text");
+    })
+    $(".bi-eye-slash-fill").click(function() {
+        $(".bi-eye-fill").fadeIn();
+        $(".bi-eye-slash-fill").hide();
+        $("#pass-input").attr('type', "password");
+    })
+})

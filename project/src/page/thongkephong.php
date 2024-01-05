6b1f9f82f6f8db2bd2e41c091e97d24af0424b26
dangchien2003 <chienboy03@gmail.com>
2024-01-05 12:53:19 +0700

    lấy thông tin thống kê từ db

--- project/src/page/thongkephong.php
+++ project/src/page/thongkephong.php
@@ -1,4 +1,6 @@
-<?php include './layout/header.php' ?>
+<?php include './layout/header.php';
+    include_once '../handle/checkAccount.php';
+?>
 <div class="row">
     <div class="col-lg-3 bg-menu">
         <?php include './layout/menu.php' ?>
@@ -11,15 +13,42 @@
             <div class="baocao">
                 <div class="thongke" id="thongkesoluongtrong">
                     <div class="data d-flex justify-content-center" id="data">
-                        <span class="group">
-                            <p><span class="option">Phòng trống</span><span>: </span><span class=data>1</span></p>
-                        </span>
-                        <span class="group">
-                            <p><span class="option">Đang ở</span><span>: </span><span class=data>1</span></p>
-                        </span>
-                        <span class="group">
-                            <p><span class="option">Khác</span><span>: </span><span class=data>1</span></p>
-                        </span>
+                        <?php
+                        $sql = "SELECT phong.tinhTrang, COUNT(phong.tinhTrang) as sl, tinhTrang.tinhTrang as tentt, (select COUNT(*) from phong) as tongphong FROM phong JOIN tinhTrang on tinhTrang.id = phong.tinhTrang GROUP BY phong.tinhTrang; ";
+                        $result = query_no_input($sql);
+                        $data = [];
+                        while ($row = $result->fetch_assoc()) {
+                            switch ($row['tinhTrang']) {
+                                case 15:
+                                    if (checkRequest($data, [$row['tentt']])) {
+                                        $data[$row['tentt']] += $row['sl'];
+                                    } else {
+                                        $data[$row['tentt']] = $row['sl'];
+                                    }
+                                    break;
+                                case 18:
+                                    if (checkRequest($data, [$row['tentt']])) {
+                                        $data[$row['tentt']] += $row['sl'];
+                                    } else {
+                                        $data[$row['tentt']] = $row['sl'];
+                                    }
+                                    break;
+                                default:
+                                    if (checkRequest($data, ['Khác'])) {
+                                        $data["Khác"] += $row['sl'];
+                                    } else {
+                                        $data["Khác"] = $row['sl'];
+                                    }
+                            }
+                        }
+                        foreach ($data as $key => $value) {
+                            ?>
+                            <span class="group">
+                                <p><span class="option"><?php echo $key;?></span><span>: </span><span class=data><?php echo $value;?></span></p>
+                            </span>
+                            <?php
+                        }
+                        ?>
                     </div>
                     <div class="bieudo justify-content-center d-flex" id="bieudo">
                         <canvas></canvas>
@@ -30,15 +59,18 @@
                 </div>
                 <div class="thongke" id="thongkesoluongtrong1">
                     <div class="data d-flex justify-content-center" id="data">
-                        <span class="group">
-                            <p><span class="option">Trống 1</span><span>: </span><span class=data>1</span></p>
-                        </span>
-                        <span class="group">
-                            <p><span class="option">Trống 2</span><span>: </span><span class=data>5</span></p>
-                        </span>
-                        <span class="group">
-                            <p><span class="option">Trống 3</span><span>: </span><span class=data>2</span></p>
-                        </span>
+                    <?php
+                        $sql = "SELECT count(slt) as sl, slt from (select (phong.sucChua-COUNT(sinhvien.maPhong)) as slt from phong LEFT JOIN sinhvien on phong.maPhong = sinhvien.maPhong where (sinhvien.tinhTrang != 1 AND phong.tinhTrang != 20) OR (sinhvien.tinhTrang is null AND phong.tinhTrang != 20) GROUP BY phong.maPhong) as tkslt GROUP BY slt";
+                        $result = query_no_input($sql);
+                        $data = [];
+                        while ($row = $result->fetch_assoc()) {
+                            ?> 
+                            <span class="group">
+                                <p><span class="option">Trống <?php echo $row['slt'];?></span><span>: </span><span class=data><?php echo $row['sl'];?></span></p>
+                            </span>
+                            <?php 
+                        }
+                        ?>
                     </div>
                     <div class="bieudo justify-content-center d-flex" id="bieudo">
                         <canvas></canvas>
@@ -53,4 +85,4 @@
 </div>
 <?php include './layout/footer.php';
 addScript(["chart", "thongke"])
-?>
+    ?>
